8ffddf292d34d14295f9c434f18f6cb3ba591179
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-06-02 20:10:41 +1000

    143. Made towers shoot, made projectiles collide with Enemy, triggered damage, triggered death and deathFX - all by myself !!!

---
+++ Assets/EnemyHits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHits : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int hits = 6;
+
+    private void OnParticleCollision(GameObject other) //(GameObject other) is a Unity default, dw about it
+    {
+        ProcessHit();
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        //consider adding hit FX
+        hits = hits - 1;
+    }
+
+    private void KillEnemy()
+    {
+        Instantiate(deathFX, transform.position, Quaternion.identity, parent);
+        Destroy(gameObject);
+    }
+}
