72d87f2dc55fafffbd77b68f42c53ac50bebaa59
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-06-05 08:07:02 +1000

    148. Moved scripts into folder. Created EnemySpawner, and used a Coroutine myself!

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] enemiesToSpawn;
+    [SerializeField] Transform parentObject;
+    [SerializeField] float secondsBetweenSpawns = 4f;
+ 
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        foreach (GameObject enemy in enemiesToSpawn)
+        {
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            Instantiate(enemy, transform.position, Quaternion.identity, parentObject);
+        }
+    }
+}
