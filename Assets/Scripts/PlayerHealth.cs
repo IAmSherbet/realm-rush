3be5ab87f0727187ce75522f64125a7338b0589d
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-06-08 18:01:26 +1000

    161. Added AudioClip for damage to PlayerHealth.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,21 +7,33 @@ public class PlayerHealth : MonoBehaviour
 {
     public int playerHealth = 10;
     [SerializeField] Text healthText;
+    [SerializeField] AudioClip playerDamageSFX;
 
     private void Start()
     {
         healthText.text = playerHealth.ToString();
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
+        PlayDamageSFX();
         ReduceHealth();
-        healthText.text = playerHealth.ToString();
+        UpdateHealthText();
+    }
+
+    private void PlayDamageSFX()
+    {
+        GetComponent<AudioSource>().PlayOneShot(playerDamageSFX);
     }
 
     public void ReduceHealth()
     {
         playerHealth = playerHealth - 1;
     }
+
+    private void UpdateHealthText()
+    {
+        healthText.text = playerHealth.ToString();
+    }
+
 }
